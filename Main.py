41b27c3e66d57a92e99623d77115083a7fbc1f92
bstrong5923 <drizzeerascoll@gmail.com>
2024-09-27 12:11:40 -0700

    output interpretation works

--- Main.py
+++ Main.py
@@ -9,23 +9,30 @@ import numpy as np
 HEIGHT = 1024
 WIDTH = 1024
 
+options = ["Stay Still", "Turn 90°", "Turn 180°", "Turn 270°", "Go Forward", "Go Right", "Go Backward", "Go Left"]
+
 class guy:
     def __init__(self):
-        self.show = Actor("red")
-    def move(self, n, e, s, w):
-        NS = 0
-        EW = 0
-        NESW = 0
-        if n > s:
-            NS = 1
-        if e > w:
-            EW = 1
-        if NS == 1:
-            if EW == 1:
-
-
+        self.show = Actor("arrow")
+    def move(self, choices):
+        done = 0
+        for x in range(len(choices)):
+            if done == 0:
+                me = 1
+                for y in range(x + 1, len(choices)):
+                    if choices[x] < choices[y]:
+                        me = 0
+                if me == 1:
+                    choice = options[x]
+                    done = 1
+        print(choice)
 
 isaac = guy()
+
+vals = [random.randint(0,1000), random.randint(0,1000), random.randint(0,1000), random.randint(0,1000), random.randint(0,1000), random.randint(0,1000), random.randint(0,1000), random.randint(0,1000)]
+print(options)
+print(vals)
+isaac.move(vals)
 
 def draw():
     screen.clear()
