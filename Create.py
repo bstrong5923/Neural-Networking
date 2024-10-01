d023a4535a0c6533a65d730b4d90a04c494326fe
bstrong5923 <158350091+bstrong5923@users.noreply.github.com>
2024-10-01 12:22:14 -0700

    Update Create.py
    added ", \n"

--- Create.py
+++ Create.py
@@ -9,11 +9,13 @@ for guy in range(guys):
     prin = ""
     for lay in range(1, layers - 1):
         for neuron in range(npl[lay]):
+            if neuron > 0: 
+                prin += ", \n"
             weights = []
             for x in range(npl[lay - 1]):
                 weights.append(random.randint(0,30) / 10)
             bias = random.randint(0, int(npl[lay - 1] / 3))
             denom = sum(weights) + bias
-            prin += str([weights, bias, denom]) + "\n"
+            prin += str([weights, bias, denom])
 
     print("guy" + str(guy) + " = guy(" + prin + ")")
