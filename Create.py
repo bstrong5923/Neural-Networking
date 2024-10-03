e6cc56e76b16de3a503a95a1f8378178ee8b1af1
bstrong5923 <drizzeerascoll@gmail.com>
2024-10-03 12:02:26 -0700

    working on creator output formatting

--- Create.py
+++ Create.py
@@ -8,13 +8,21 @@ for x in range(layers):
 for guy in range(guys):
     prin = ""
     for lay in range(1, layers):
+        prin += "NEW LAYER"
+        if lay != 1:
+            prin += "], "
+        prin += "["
         for neuron in range(npl[lay]):
+            if neuron != 0:
+                prin += "], ["
             weights = []
             for x in range(npl[lay - 1]):
-                weights.append(random.randint(0,30) / 10)
+                if x != 0:
+                    prin += ", "
+                add = random.randint(0,30) / 10
+                weights.append(add)
+                prin += str(add)
             bias = random.randint(0, int(npl[lay - 1] / 3))
             denom = sum(weights) + bias
-            ", ".join(weights)
-            prin += str([weights, bias, denom])
 
     print("guy" + str(guy + 1) + " = guy(" + prin + ")")
